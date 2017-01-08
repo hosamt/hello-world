8232a4115c7d95161ea21bd22c82bcbe757e04ac
hosamt <tarakjico@gmail.com>
2017-01-08 07:18:06 +0200

    Add files via upload

---
+++ 123temp.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+  $("button").click(function(){
+    $("h2").hide();
+  }
+  $("button#n").click(function(){
+    $("p").hide();
+  }
+  );
+});
+</script>
+</head>
+
+<body>
+<h2>This is a heading</h2>
+<p>This is a paragraph.</p>
+<p>This is another paragraph.</p>
+<button>Click me</button><br>
+<button id="n">Click me 2</button>
+</body>
+</html>
